4c6aa53b3ab0032a57f60264cbb06a8fb52f0375
Saeed Noor <163897168+saeednoor786@users.noreply.github.com>
2024-05-23 21:59:00 +0500

    nine assignment

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+//Q:9 Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+// create a message that reveals your favorite number. Print that message.
+let favorite_Number = 10;
+console.log(`my favorite number is , ${favorite_Number}`);
+//Q:10 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+//If you don’t have anything specific to write because your programs are too simple at this point, just 
+// add your name and the current date at the top of each program file. 
+// Then write one sentence describing what the program does.
+// saeed_noor
+// friday/03/20
+// store my favorite number in a variable
+const favoriteNumber = 50;
+// creat a message with favorite number 
+const message = (`my favoriteNumber is , ${favoriteNumber}`);
+console.log(message);
---
+++ app.ts
@@ -0,0 +1,10 @@
+
+//Q:9 Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+// create a message that reveals your favorite number. Print that message.
+
+let favorite_Number:number = 10;
+console.log(`my favorite number is , ${favorite_Number}`);
+
+
+
+
